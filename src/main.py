647a6df7e6e7a2bedcfe0d012f9cab82c8196cc0
Rui Antunes <rui.andre.antunes@gmail.com>
2022-12-17 15:33:02 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,70 @@
+import psutil
+import subprocess
+import tomllib
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+PROCESS = "nvstreamer.exe"
+
+
+@dataclass
+class Settings:
+    executable_path = None
+    profile_path = None
+    tv_profile = None
+    remote_desktop_profile = None
+
+    def set_config(self, config):
+        self.executable_path = Path(config["executable"]["path"])
+        self.profile_path = Path(config["profile"]["path"])
+        self.tv_profile = self.profile_path / Path(config["profile"]["name"]["tv"])
+        self.desktop_profile = self.profile_path / Path(config["profile"]["name"]["desktop"])
+
+@dataclass
+class Commands:
+    tv: str = ""
+    desktop: str = ""
+    extended_displays: str = ""
+
+    def make_commands(self, settings):
+        self.tv = f"{settings.executable_path} -load:{settings.tv_profile}"
+        self.desktop = f"{settings.executable_path} -load:{settings.desktop_profile}"
+
+
+def load_config():
+    current_dir = Path(__file__).resolve().parent
+    config_path = current_dir.joinpath("config.toml")
+    with open(config_path, "rb") as config_file:
+        config = tomllib.load(config_file)
+    return config
+
+
+def main():
+    settings = Settings()
+    config = load_config()
+    settings.set_config(config)
+
+    commands = Commands()
+    commands.make_commands(settings)
+
+    subprocess.run(commands.tv)
+    while True:
+        streaming = False
+        for process in psutil.process_iter(["name"]):
+            if process.name() == PROCESS:
+                streaming = True
+                pid = process.pid
+                break
+        if streaming:
+            nvidia_process = psutil.Process(pid)
+            subprocess.run(commands.desktop)
+            nvidia_process.wait()
+            subprocess.run(commands.tv)
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
